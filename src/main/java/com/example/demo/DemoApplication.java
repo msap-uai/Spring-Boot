c5903e090a9d38f25679d8539ec7a1039c8174f9
Manuel Saponaro <manuelsaponaro.uai@gmail.com>
2022-06-19 09:24:56 -0300

    Scheduling_Tasks
    creando tareas programadas

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //habilita  ejecutor de tareas en segundo plano
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/demo/SchedulingTasks/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package com.example.demo.SchedulingTasks;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 30000) // 5000 = 5 segundos
+    //intervalo entre invocaciones de métodos. Hay otras opciones , como fixedDelay
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+        //imprime la hora
+    }
+}
+
